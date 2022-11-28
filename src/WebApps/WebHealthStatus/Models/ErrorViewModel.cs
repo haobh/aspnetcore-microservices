47e50bc3dac30c798b01a965606f7419ca82377f
haobh <haobh@vnpt.vn>
2022-11-28 17:23:05 +0700

    first commit

---
+++ src/WebApps/WebHealthStatus/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebHealthStatus.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
